250c98f6587c7b10016fb7f55cba5aae2dd3d7ea
vikrantpandita <vikrantpandita002@gmail.com>
2021-08-01 00:50:47 +0530

    Add files via upload

---
+++ Question3.py
@@ -0,0 +1,11 @@
+import subprocess
+import os
+dw = subprocess.check_output(['netsh','wlan','show','network'])                           # using the check_output() for having the network term retrival
+decoded_dw = dw.decode('ascii')                                                           # decode it to strings
+print(decoded_dw)                                                                         # displaying the information                                                                                                 
+ssid = input('Enter Name/SSID of the Wifi Network you wish to connect to: ')    # input Wifi name  
+# connect to the given wifi network
+#os.system(f'''cmd /c "netsh wlan connect name={name_of_router}"''')
+passwd = input('Enter password')
+output = subprocess.check_output(['sudo', 'nmcli', 'device', 'wifi', 'connect', ssid,'password', passwd])
+
